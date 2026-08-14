16e97089daf1ed23fd8dd5a26338612a1736817c
CesarRomeroDev <cesar_romero.dev@outlook.com>
2026-08-13 21:42:06 -0600

    Se modifica la ruta de las imagenes de la vista detalles a by: se realiza movimiento de translate en imagenes en vista tasting y project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,7 +21,7 @@ export const routes: Routes = [
         loadComponent: () => import('./characters/pages/work/work.component')
       },
       {
-        path: ':slug',
+        path: 'by/:slug',
         title: '',
         loadComponent: () => import('./characters/components/work-details/work-details.component')
 
